ad01cac7eaa83c072bcb181ff205493990e73435
WazupGuy <91200330+WazupGuy@users.noreply.github.com>
2022-12-15 21:42:39 +0100

    sol pref

--- Debat_Tanguy_Lavallee_Ciavaldini/src/up/mi/ttsmmc/Solution_pref.java
+++ Debat_Tanguy_Lavallee_Ciavaldini/src/up/mi/ttsmmc/Solution_pref.java
@@ -1,31 +1,49 @@
-package up.mi.ttsmmc;
-
 import java.util.List;
 
 public class Solution_pref {
 
+
 	public List<List<ArgumentNoeud>>  getSolPref (List<List<ArgumentNoeud>> sol_admi) {
-		List<List<ArgumentNoeud>> sol_pref = null;
+		List<List<ArgumentNoeud>> sol_pref = new ArrayList<>();
 		boolean est_contenu = false;
 		for( List<ArgumentNoeud> element : sol_admi) {
 			int taille = element.size();
 			if(taille==0) {
 				continue;// si ensemble vide passage au prochain element
 			}
-			
-			else {
-				
-				for (List<ArgumentNoeud> compare : sol_admi) {
-					if(compare.equals(element)) {
+			if (taille==1) {
+				for(List<ArgumentNoeud> cmp : sol_admi) {
+					if(element.equals(cmp)) {
 						continue;
 					}
-					if(compare.contains(element)) {
+					if(cmp.contains(element)) {
 						est_contenu = true;
-						break; //arret de la seconde boucle for, l'argument element n est pas une solution preferable
+						break;
 					}
-					
 				}
 			}
+			else {
+				if(compare.equals(element)) {
+						continue;
+					}
+			}
+		
+				
+				for (List<ArgumentNoeud> compare : sol_admi) {
+					
+					
+					/*for(List<ArgumentNoeud> noeud : sol_admi ) {*/
+						if(compare.contains(element)) {
+						est_contenu = true;
+						break; //arret de la seconde boucle for, l'argument element n est pas une solution preferable
+						}
+					}
+					
+					
+					
+					
+				}
+			
 			
 			if(est_contenu == false) {
 				sol_pref.add(element);
